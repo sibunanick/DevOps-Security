52aea074af4301483713fb9d313aed7a7c7f5b09
Nick beun <nickbeun@icloud.com>
2024-09-23 12:37:42 +0200

    Update quoter_templates.py

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">Inloggahhh!</label>
+<label class="link" for="signinCheckbox">Inloggahhdasdsdsah!</label>
 """
 
     return f"""<!DOCTYPE html>
